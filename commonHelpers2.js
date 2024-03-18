5f380900df64b75f94a06b9bd3c58cba47f6bdb1
999Valeriy999 <143195865+999Valeriy999@users.noreply.github.com>
2024-03-18 16:26:52 +0000

    Deploying to gh-pages from @ 999Valeriy999/GOIT-JS-HW-10@fd7ff2abad3bdc00c0ba4f78bd73c9a08ae6951d 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-f3635d75.js";import{i}from"./assets/vendor-77e16229.js";const s=document.querySelector(".form");s.addEventListener("submit",l=>{l.preventDefault();const t=parseInt(s.elements.delay.value,10),m=s.elements.state.value;s.elements.delay.value="",new Promise((e,r)=>{setTimeout(()=>{m==="fulfilled"?e(t):r(t)},t)}).then(e=>{i.success({title:"Fulfilled promise",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{i.error({title:"Rejected promise",message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=commonHelpers2.js.map
